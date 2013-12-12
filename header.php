6102d0916aac9645e9993b1392f800ce8af27162
Kennedy <kennedysgarage@gmail.com>
2013-12-12 01:00:33 -0500

    init commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Brian Revak</title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="/css/bootstrap.css">
+  <link rel="stylesheet" href="/css/ss-standard.css">
+  <link rel="stylesheet" href="/css/ss-social-circle.css">
+  <link rel="stylesheet" href="/css/main.css">
+
+  <script src="/js/modernizr.min.js"></script>
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-46399611-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'header.php'; ?>
+
+
+<img src="/img/water.jpg" class="bg">
+
+<div id="page-wrap">
+  <img src="/img/brian-revak.png">
+  <h1>Brian Revak</h1>
+  <ul id="social">
+    <li><a href=""><i class="ss-icon ss-social-circle">&#xF611;</i></a></li>
+    <li><a href=""><i class="ss-icon ss-social-circle">&#xF612;</i></a></li>
+    <li><a href=""><i class="ss-icon ss-social-circle">&#xF671;</i></a></li>
+    <li><a href=""><i class="ss-icon ss-social-circle">&#x2709;</i></a></li>
+  </ul>
+</div><!--/page-wrap-->
+
+
+<?php include("footer.php"); ?>
